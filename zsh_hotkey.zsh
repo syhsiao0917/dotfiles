bcbeaef47237e5596a9d30c1bcac60f45dcfd572
syhsiao0917 <syhsiao0917@gmail.com>
2017-11-22 14:48:12 +0800

    add zsh_hotkey

---
+++ zsh_hotkey.zsh
@@ -0,0 +1,68 @@
+
+
+setopt hist_ignore_space 
+alias cd=" cd" 
+alias ls=" ls"
+
+
+## control+s as ls 
+
+stty -ixon ## disable control+s as suspend 
+
+function _control_s { 
+	buf=$BUFFER
+	cur=$CURSOR
+
+	BUFFER='ls ' 
+	CURSOR=3
+	zle list-choices
+
+	BUFFER=$buf
+	CURSOR=$cur
+
+} 
+
+zle -N _control_s
+bindkey '^s'  _control_s
+
+## get pwd 
+function _get_pwd { 
+	zle -U $(pwd) 
+} 
+
+## for alt+backspace on path 
+export WORDCHARS='*?_[]~=&;!#$%^(){}'
+
+# tip: alt+backspace to delete path 
+# tip: ctrl+y to undo delete
+
+zle -N _get_pwd
+bindkey '^[d' _get_pwd
+
+## directory up 
+function _dir_up { 
+   cd .. 
+   zle reset-prompt 
+}
+zle -N _dir_up
+bindkey '^[e' _dir_up
+
+## 
+function _mkdir_cd()  { 
+	mkdir $1 
+	cd $1
+}
+
+alias md='_mkdir_cd'
+
+### insert argument 
+bindkey '^r' insert-last-word
+autoload -Uz copy-earlier-word
+zle -N copy-earlier-word
+bindkey "^u" copy-earlier-word
+
+# tip: first use control+r to insert last argument 
+# tip: next control+r will insert previous command line last argument 
+# tip: control+r will insert previous argument 
+
+
